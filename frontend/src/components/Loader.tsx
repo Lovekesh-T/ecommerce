5ef5920996867216d67808b8cb23dd201a5eba1a
king100xp <damnbooyee@gmail.com>
2024-08-02 19:20:09 +0530

    update loader

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,5 +1,9 @@
 const Loader = () => {
-  return  <div className="loader-screen"></div>
+  return  <div className="overlay">
+  <div className="overlay__inner">
+      <div className="overlay__content"><span className="spinner"></span></div>
+  </div>
+</div>
 
 };
 
